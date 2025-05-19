a3dc817ccc147a0fd6232926f99d158c081fac4c
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2025-05-19 21:55:35 +0530

    Update jwt_config.py

--- backend/jwt_config.py
+++ backend/jwt_config.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
 
-JWT_SECRET_KEY = "gK5t8s3Wv9z0Xy2Lm5Np6Qr8Bj7Hu1Ds" 
+# JWT_SECRET_KEY = "gK5t8s3Wv9z0Xy2Lm5Np6Qr8Bj7Hu1Ds" 
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
